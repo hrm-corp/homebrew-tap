d3c0a5e8934105ae1c65bcb1228c8f0bc40527fe
probe <re2rar@gmail.com>
2024-07-19 14:23:41 +0900

    add version and sha256

--- Formula/kic-iam-auth.rb
+++ Formula/kic-iam-auth.rb
@@ -3,9 +3,10 @@ class KicIamAuth < Formula
   homepage "https://github.com/hrm-corp/kic-iam-auth"
   on_macos do
      url "https://objectstorage.kr-central-2.kakaocloud.com/v1/fe631cd1b7a14c0ba2612d031a8a5619/public/docs/binaries-kic-iam-auth/Mac%20ARM_64%2064Bit/kic-iam-auth"
-     sha256 "your_sha256_checksum_for_mac_arm"
+     sha256 "5136d1d56f31e92a8a1ee69fae2bcb1d5e16b6b8a439c32bad72219ee725c5b4"
   end
 
+  version "0.1.2"
   license "Apache-2.0"
   head "https://github.com/hrm-corp/kic-iam-auth.git", branch: "main"
 
